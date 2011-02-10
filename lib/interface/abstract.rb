f4e14fef25b4521f9b238bba7abdc53e06c331e8
Sean Huber <shuber@huberry.com>
2011-02-10 01:59:08 -0800

    rename Interface::Abstract.extended(base) variable to interface and update documentation

--- lib/interface/abstract.rb
+++ lib/interface/abstract.rb
@@ -1,14 +1,14 @@
 module Interface
-  # When extended, this abstract interface will re-define an object's methods to raise <tt>NotImplementedError</tt> when called (unless handled by <tt>method_missing</tt>)
+  # When extended, this module will re-define an interface's methods to raise <tt>NotImplementedError</tt> when called (unless handled by <tt>method_missing</tt>)
   module Abstract
-    def self.extended(base) # :nodoc:
-      base.class_eval do
+    def self.extended(interface) # :nodoc:
+      interface.class_eval do
         instance_methods(false).each do |method|
           define_method(method) do |*args, &block|
             begin
               method_missing(method.to_sym, *args, &block)
             rescue NoMethodError
-              raise NotImplementedError.new("#{self.class} needs to implement '#{method}' for interface #{base}")
+              raise NotImplementedError.new("#{self.class} needs to implement '#{method}' for interface #{interface}")
             end
           end
         end
